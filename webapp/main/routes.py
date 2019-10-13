a60c73b5e07a461f1ca4d010b000574e72e17932
joshua-vegas <vegas.joshua@gmail.com>
2019-10-13 13:01:52 +0200

    Beta version of admin validation

--- webapp/main/routes.py
+++ webapp/main/routes.py
@@ -3,6 +3,10 @@ from werkzeug.utils import redirect
 
 from webapp.main import bp
 from flask import render_template, request, session
+from webapp.bdd.models.users import Manager
+from webapp.bdd.models.requests import OpenAccountRequest
+
+from webapp.extensions import db
 
 
 @bp.route('/')
@@ -13,24 +17,41 @@ def index():
 
 
 @bp.route('/manager')
-@bp.route('/home/manager')
-@bp.route('/index/manager')
+@bp.route('/manager/home')
+@bp.route('/manager/index')
 @login_required
 def index_manager():
     return render_template('manager/index.html')
 
 
 @bp.route('/admin')
-@bp.route('/home/admin')
-@bp.route('/index/admin')
+@bp.route('/admin/home')
+@bp.route('/admin/index')
 @login_required
 def index_admin():
     return render_template('admin/index.html')
 
 
+@bp.route('/admin/requests')
+@login_required
+def requests():
+    requests = OpenAccountRequest.query.filter(OpenAccountRequest.manager_id == '0').all()
+    list_manager =  db.session.query(Manager.id, Manager.firstname).all()
+    return render_template("admin/requests.html",
+                           title='List of requests',
+                           requests=requests,
+                           list_manager=list_manager)
+
+@bp.route('/admin/set_manager', methods=['GET', 'POST'])
+@bp.endpoint('set_manager')
+def set_manager():
+    o = request.form
+    print(o)
+    return redirect(request.referrer)
+
 @bp.route('/client')
-@bp.route('/home/client')
-@bp.route('/index/client')
+@bp.route('/client/home')
+@bp.route('/client/index')
 @login_required
 def index_client():
     return render_template('client/index.html')
